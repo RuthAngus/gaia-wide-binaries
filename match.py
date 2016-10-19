ce9c3081ffcaa2ca3da96ae1d10fb297b50f15c7
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-19 09:51:26 -0400

    removing problem with uniqueness

--- match.py
+++ match.py
@@ -26,29 +26,77 @@ kplr_tgas = pd.read_csv("kic_tgas_mod.csv")
 epic_tgas = pd.read_csv("epic_tgas_mod.csv")
 
 # find the indices of the star1s and 2s that are in kepler-tgas
-m = star1_df.source_id.isin(kplr_tgas.source_id).values * \
+m_tgas = star1_df.source_id.isin(kplr_tgas.source_id).values * \
     star2_df.source_id.isin(kplr_tgas.source_id).values
-star1_kic = star1_df.iloc[np.where(m)]  # list of star1s in kplr-tgas
-star2_kic = star2_df.iloc[np.where(m)]  # list of star1s in kplr-tgas
+star1_tgas = star1_df.iloc[np.where(m_tgas)[0]]
+star2_tgas = star2_df.iloc[np.where(m_tgas)[0]]
+print(len(np.where(m_tgas)[0]), "pairs found in kepler")
+
+print(len(star1_tgas.source_id))
+print(len(np.unique(star1_tgas.source_id.values)))
+print(len(star2_tgas.source_id))
+print(len(np.unique(star2_tgas.source_id.values)))
+
+# find the indices of the star1s and 2s that are in epic-tgas
+m_tgas_epic = star1_df.source_id.isin(epic_tgas.source_id).values * \
+    star2_df.source_id.isin(epic_tgas.source_id).values
+star1_tgas_epic = star1_df.iloc[np.where(m_tgas_epic)[0]]
+star2_tgas_epic = star2_df.iloc[np.where(m_tgas_epic)[0]]
+print(len(np.where(m_tgas_epic)[0]), "pairs found in K2")
+
+print(len(star1_tgas_epic.source_id))
+print(len(np.unique(star1_tgas_epic.source_id.values)))
+print(len(star2_tgas_epic.source_id))
+print(len(np.unique(star2_tgas_epic.source_id.values)))
+
+star1_kic = pd.merge(star1_tgas, kplr_tgas, on=star1_tgas.columns.tolist(),
+                     how='left')
+star2_kic = pd.merge(star2_tgas, kplr_tgas, on=star2_tgas.columns.tolist(),
+                     how='left')
+star1_epic = pd.merge(star1_tgas_epic, epic_tgas,
+                      on=star1_tgas.columns.tolist(), how='left')
+star2_epic = pd.merge(star2_tgas_epic, epic_tgas,
+                      on=star2_tgas.columns.tolist(), how='left')
+print(np.shape(star1_kic), np.shape(star2_kic), np.shape(star1_epic),
+      np.shape(star2_epic))
+assert 0
+
+m1_kplr = kplr_tgas.source_id.isin(star1_tgas.source_id).values
+m2_kplr = kplr_tgas.source_id.isin(star2_tgas.source_id).values
+star1_kic = kplr_tgas.iloc[np.where(m1_kplr)[0]]
+star2_kic = kplr_tgas.iloc[np.where(m2_kplr)[0]]
+print(len(np.where(m1_kplr)[0]), "pairs found again in kepler")
+print(len(np.where(m2_kplr)[0]), "pairs found again in kepler")
 print(np.shape(star1_kic))
 print(star1_kic.keys())
 
 plt.clf()
-for i, _ in enumerate(star1_kic.ra):
+for i, _ in enumerate(star1_kic.ra.values):
     plt.plot([star1_kic.ra.values[i], star2_kic.ra.values[i]],
              [star1_kic.dec.values[i], star2_kic.dec.values[i]])
 plt.plot(star1_kic.ra, star1_kic.dec, "r.")
 plt.plot(star2_kic.ra, star2_kic.dec, "k.")
 plt.savefig("kepler_ra_dec")
+assert 0
 
 # do the same for the epic
-m = star1_df.source_id.isin(epic_tgas.source_id).values * \
+m_tgas = star1_df.source_id.isin(epic_tgas.source_id).values * \
     star2_df.source_id.isin(epic_tgas.source_id).values
-star1_epic = star1_df.iloc[np.where(m)]  # list of star1s in kplr-tgas
-star2_epic = star2_df.iloc[np.where(m)]  # list of star1s in kplr-tgas
+star1_tgas = star1_df.iloc[np.where(m_tgas)]
+star2_tgas = star2_df.iloc[np.where(m_tgas)]
+m1_epic = kplr_tgas.source_id.isin(star1_tgas.source_id).values
+m2_epic = kplr_tgas.source_id.isin(star2_tgas.source_id).values
+star1_epic = kplr_tgas.iloc[np.where(m1_epic)]
+star2_epic = kplr_tgas.iloc[np.where(m2_epic)]
+
 print(np.shape(star1_epic))
 
 plt.clf()
+for i, _ in enumerate(star1_epic.ra.values):
+    plt.plot([star1_epic.ra.values[i], star2_epic.ra.values[i]],
+             [star1_epic.dec.values[i], star2_epic.dec.values[i]])
 plt.plot(star1_epic.ra, star1_epic.dec, "k.")
 plt.plot(star2_epic.ra, star2_epic.dec, "k.")
+# plt.xlim(230, 270)
+# plt.ylim(-30, -12)
 plt.savefig("k2_ra_dec")
